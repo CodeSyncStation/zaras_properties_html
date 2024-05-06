cea1fca82dc24b54e6577b3ba4e220b28e64bc3d
Naim Ahmad <naimahmad201103@gmail.com>
2024-05-06 15:11:35 +0600

    completed hero section

---
+++ libs/swiper/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".about-slider", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".about-slide", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
