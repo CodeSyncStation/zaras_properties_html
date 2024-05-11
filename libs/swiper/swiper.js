e7d86e7fe2c36ee965a121793e90154aeca1ecec
Naim Ahmad <naimahmad201103@gmail.com>
2024-05-11 14:57:33 +0600

    changed some animation

--- libs/swiper/swiper.js
+++ libs/swiper/swiper.js
@@ -1,3 +1,23 @@
+const heroSlider = new Swiper(".heroSlider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  next: {
+    translate: ["100rem"],
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
 const aboutSlider = new Swiper(".about-slider", {
   spaceBetween: 10,
   slidesPerView: 4,
