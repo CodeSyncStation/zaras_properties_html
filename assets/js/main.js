93007a07ea31ffda29460747d9ef25241679d90f
Naim Ahmad <naimahmad201103@gmail.com>
2024-05-11 15:48:03 +0600

    fixed navbar allignment

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,10 @@ function previewImage(imageURL) {
   previewImg.src = imageURL;
   bsModal.show();
 }
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
